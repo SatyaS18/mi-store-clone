4e9134a40204f43c859fb56e795cb78b8945f829
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2022-11-20 01:06:12 +0530

    added carousel

--- mi-store-clone/src/App.js
+++ mi-store-clone/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar.js";
 import { BrowserRouter as Router } from "react-router-dom";
+import Slider from "./components/Slider.js";
+import Data from "./data/data.json";
 
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
+      <Slider start={Data.banner.start} />
     </Router>
   );
 }

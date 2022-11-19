059fac9790f57a558aa8cbe74226ff73baa5f604
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2022-11-20 01:38:45 +0530

    made offers section

--- mi-store-clone/src/App.js
+++ mi-store-clone/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import Slider from "./components/Slider.js";
 import Data from "./data/data.json";
+import Offers from "./components/Offers.js";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <PreNavbar />
       <Navbar />
       <Slider start={Data.banner.start} />
+      <Offers offers={Data.offer} />
     </Router>
   );
 }

3d5f0566b00d052aefdcbea257fe9479e85a8519
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2022-11-20 14:38:36 +0530

    added hot item card and hot accessories

--- mi-store-clone/src/App.js
+++ mi-store-clone/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Slider from "./components/Slider.js";
 import Data from "./data/data.json";
 import Offers from "./components/Offers.js";
 import Heading from "./components/Heading.js";
 import StarProduct from "./components/StarProduct.js";
 import HotAccessoriesMenu from "./components/HotAccessoriesMenu.js";
+import HotAccessories from "./components/HotAccessories.js";
 
 function App() {
   return (
@@ -20,6 +21,18 @@ function App() {
       <StarProduct starProduct={Data.starProduct} />
       <Heading text="Hot Accessories" />
       <HotAccessoriesMenu />
+      <Routes>
+        <Route
+          exact
+          path="/music"
+          element={
+            <HotAccessories
+              music={Data.hotAccessories.music}
+              musicCover={Data.hotAccessoriesCover.music}
+            />
+          }
+        ></Route>
+      </Routes>
     </Router>
   );
 }
